c0860064a6ec868b8f5491fe736c27acf460240d
i04cvss <camil.hernandezs@duocuc.cl>
2026-05-11 20:50:59 -0400

    test backend pipeline

--- backend/server.js
+++ backend/server.js
@@ -133,7 +133,7 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok", message: "Backend de tienda de perritos en ejecución." });
 });
 
-console.log("Pipeline CI/CD backend funcionandoo");
+console.log("Pipeline CI/CD backend funcionando");
 // Iniciar servidor
 app.listen(PORT, async () => {
   console.log(`Servidor backend escuchando en puerto ${PORT}`);
